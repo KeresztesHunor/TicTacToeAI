89d6dc2afcb81b0191650e536ff6b833e4e7171d
KeresztesHunor <kereszteshunor36@gmail.com>
2023-10-14 13:01:21 +0200

    x/o

--- MVC/View/JatekTerView.js
+++ MVC/View/JatekTerView.js
@@ -1,8 +1,17 @@
+import { paratlanTag, parosTag } from "../../htmlUtils.js";
 import MezoView from "./MezoView.js";
 
 class JatekTerView
 {
     #mezok;
+
+    static x = parosTag("svg", { viewBox: "0 0 4 4", width: "100%", height: "100%" }, [
+        paratlanTag("polygon", { points: "0,1 1,0 2,1 3,0 4,1 3,2 4,3 3,4 2,3 1,4 0,3 1,2" })
+    ]);
+
+    static o = parosTag("svg", { width: "100%", height: "100%" }, [
+        paratlanTag("circle", { cx: "50%", cy: "50%", r: "50%" })
+    ]);
 
     constructor(szuloElem)
     {
@@ -26,9 +35,9 @@ class JatekTerView
             switch (mezoErtek)
             {
                 case 1:
-                    return "X";
+                    return JatekTerView.x;
                 case -1:
-                    return "O"
+                    return JatekTerView.o;
                 default:
                     return "";
             }
